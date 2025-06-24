b5c9e60219a0bb80f1e127453803fb6079f93fae
qxz3uc1 <marius.cornea@accenture.com>
2025-06-24 11:33:51 +0300

    some changes

--- learning/src/App.jsx
+++ learning/src/App.jsx
@@ -2,33 +2,48 @@ import { useState } from 'react'
 import Display from './Display.jsx'
 import Button  from './Button.jsx'
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState("")
+  const [total, setTotal]=useState(0)
 
   const handleLeftClick = () => {
-    const newClicks = { 
-      left: clicks.left + 1, 
-      right: clicks.right 
-    }
-    setClicks(newClicks)
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(left+1)
+    setTotal(updatedLeft + right)
   }
 
   const handleRightClick = () => {
-    const newClicks = { 
-      left: clicks.left, 
-      right: clicks.right + 1 
-    }
-    setClicks(newClicks)
+    setAll(allClicks.concat('R'));
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    setTotal(left + updatedRight);
   }
 
   return (
     <div>
-      {clicks.left}
+      {left}
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      {right}
+      <p>{allClicks}</p>
+      <p> the total is: {total}</p>
     </div>
   )
 }
